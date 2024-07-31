8604ddddaf92056407cd446d3f90a5a275beefd0
suflors <iammagical.space@gmail.com>
2024-07-31 12:17:15 -0700

    Added Discord bot.

---
+++ src/AtelierTomato.SimpleDiscordMarkovBot.Core/DiscordEventDispatcher.cs
@@ -0,0 +1,38 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+
+namespace AtelierTomato.SimpleDiscordMarkovBot.Core
+{
+	public class DiscordEventDispatcher
+	{
+		private readonly ILogger<DiscordEventDispatcher> logger;
+		private readonly DiscordSocketClient client;
+
+		public DiscordEventDispatcher(ILogger<DiscordEventDispatcher> logger, DiscordSocketClient client)
+		{
+			this.logger = logger;
+			this.client = client;
+
+			this.client.Ready += this.Client_Ready;
+
+			this.client.MessageReceived += this.Client_MessageReceived;
+		}
+
+		private async Task Client_Ready()
+		{
+			await this.client.SetGameAsync("Placeholder!", type: Discord.ActivityType.Competing);
+		}
+
+		private async Task Client_MessageReceived(SocketMessage messageParam)
+		{
+			// Don't process the command if it was a system message
+			if (messageParam is not SocketUserMessage message)
+			{
+				return;
+			}
+
+			var context = new SocketCommandContext(this.client, message);
+		}
+	}
+}
---
+++ src/AtelierTomato.SimpleDiscordMarkovBot.Service/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using AtelierTomato.SimpleDiscordMarkovBot.Core;
+using AtelierTomato.SimpleDiscordMarkovBot.Service;
+using Discord;
+using Discord.WebSocket;
+
+IHost host = Host.CreateDefaultBuilder(args)
+	.UseSystemd()
+	.ConfigureAppConfiguration((hostContext, builder) =>
+	{
+		// Add other providers for JSON, etc.
+
+		// only use user secrets when debugging.
+		if (Debugger.IsAttached)
+		{
+			builder.AddUserSecrets<Program>();
+		}
+	})
+	.ConfigureLogging((hostContext, builder) =>
+	{
+		// if we're in fact using systemd, throw out the default console logger and only use the systemd journal
+		if (Microsoft.Extensions.Hosting.Systemd.SystemdHelpers.IsSystemdService())
+		{
+			builder.ClearProviders();
+			builder.AddJournal(options => options.SyslogIdentifier = hostContext.Configuration["SyslogIdentifier"]);
+		}
+	})
+	.ConfigureServices(services =>
+	{
+		services.AddHostedService<Worker>();
+
+		var discordSocketConfig = new DiscordSocketConfig
+		{
+			// request all unprivileged but unrequest the ones that keep causing log spam
+			GatewayIntents = GatewayIntents.AllUnprivileged & ~GatewayIntents.GuildInvites & ~GatewayIntents.GuildScheduledEvents | GatewayIntents.MessageContent,
+		};
+		var client = new DiscordSocketClient(config: discordSocketConfig);
+
+		services.AddSingleton(client);
+		services.AddSingleton<DiscordEventDispatcher>();
+	})
+	.Build();
+
+await host.RunAsync();
+
+//var builder = Host.CreateApplicationBuilder(args);
+//builder.Services.AddHostedService<Worker>();
+
+//var host = builder.Build();
+//host.Run();
